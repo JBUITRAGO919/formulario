a62977eb19d28c65ea3c8d730a685d1d1d9f307f
jbuitrago <jbuitrago919@misena.edu.co>
2020-12-11 21:45:57 -0500

    creación de repositorio

---
+++ js/formulario.js
@@ -0,0 +1,19 @@
+function validar(){
+    var correo = document.getElementById("correoAcceso").value;
+    var contraseña = document.getElementById("passwordAcceso").value;
+
+    if (correo =="jbuitrago919@misena.edu.co" && contraseña == 1234) {
+        console.log("Bienvenido")
+
+        location.
+        
+        
+    } else {
+        
+        alert("password incorrecto")
+        
+    }
+
+
+
+};
